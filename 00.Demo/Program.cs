1d1289f766621c0cbe41f83bf88837f2f1cfd799
AngeloidAlpha <angeloid_alpha@abv.bg>
2024-07-06 05:55:06 +0300

    6th July 1st

--- 00.Demo/Program.cs
+++ 00.Demo/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// C#13 има Primary constructors
+// public class Elf(string username, int level) : Hero (username, level)
+// remove unnacesery usings (горе дет се появяват)
+// Ctrl + R и после Ctrl + G 
+// Ctrl + click -> go to definition - ще ни връща към описанието на метода в C#
+// стилови преференции си ги създавамне на проектно ниво файл който после ще ни подсеща 
+// как да си пишем кода за да е разбираем за екипа
--- 02.Exercise/01.Inheritance/PlayersAndMonsters/Hero.cs
+++ 02.Exercise/01.Inheritance/PlayersAndMonsters/Hero.cs
@@ -2,13 +2,7 @@
 {
     public class Hero
     {
-        // C#13 има Primary constructors
-        // public class Elf(string username, int level) : Hero (username, level)
-        // remove unnacesery usings (горе дет се появяват)
-        // Ctrl + R и после Ctrl + G 
         // this.Username използването на this е хубаво за да знаем че сочим към елемент на класа
-        // стилови преференции си ги създавамне на проектно ниво файл който после ще ни подсеща 
-        // как да си пишем кода за да е разбираем за екипа
         public Hero(string username, int level)
         {
             Username = username;
