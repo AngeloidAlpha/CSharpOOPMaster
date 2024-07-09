332ce8777e4828391e66f562650324ef60bba7f0
AngeloidAlpha <angeloid_alpha@abv.bg>
2024-07-09 06:00:24 +0300

    09.July 1st

--- 00.Demo/Program.cs
+++ 00.Demo/Program.cs
@@ -5,3 +5,43 @@
 // Ctrl + click -> go to definition - ще ни връща към описанието на метода в C#
 // стилови преференции си ги създавамне на проектно ниво файл който после ще ни подсеща 
 // как да си пишем кода за да е разбираем за екипа
+// Ctrl + M + O закрива всичко
+// Ctrl + M + E отваря всичко
+
+using _00.Demo;
+
+// Lab Lesson Encapsulation
+
+
+Student student = new("Jurgen Klopp", 50);
+// при капсулиран клас ще ни изпише грешка
+// student.Name = "Jose Mourinho"; // public string Name { get; private set; }
+// ще може да променим името на студента, но в доста случаи няма да искаме
+
+// ще може да влезем и променим името на студента чрез метод в класа
+// как ще променим годините на студента и видим дали е валидно?
+try
+{
+    // защото Age е protected в Student вече мога да го достъпя само там от неговите наследници
+    // student.Age = -151;
+}
+catch (Exception)
+{
+    Console.WriteLine("Tryed to change to an invalid Age");
+}
+// защото се опитахме да променим годините но не са валидни то не ги променихме
+// по този начин капсулирахме до някаква степен кода
+// даваме правилата на кода поради смисъла и контекста на използване
+// за годините например трябва да не са по-малки от 18 години, да не са отрицателни, да не са прекалено големи и т.нат.
+
+// искам да поставя оценки на нашия студент, но не всякакви а такива които аз искам
+student.AddAvarageGrade("Math", 4.6);
+student.AddAvarageGrade("BEL", 3.1);
+// защото нямаме get и set тук няма да ни позволи да добавяме нещо невалидно което заобикаля условията ни
+// student.AvarageGrades.Add("BEL", 15.20);
+Console.WriteLine(student.Name);
+Console.WriteLine(student.Age);
+student.PrintAvarageGrade();
+
+Student studentCurrent = student.GetCurrent();
+
---
+++ 00.Demo/Student.cs
@@ -0,0 +1,132 @@
+﻿using _00.Demo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _00.Demo
+{
+    // некапсулиран клас всичко е достъпно от външния свят
+    // в програмирането нещо което не е разрешено е позволено (т.е. експлатира се)
+    /*
+        public class Student
+    {
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            AvarageGrades = new Dictionary<string, double>();
+        }
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public Dictionary<string, double> AvarageGrades { get; set; }
+    }
+    */
+
+    // ако изпишем класа да е internal class Student той тогава ще иска класове само в scope-a 00.Demo
+    public class Student
+    {
+        // за да премахнем изцяло възможноста за променяне на имената на студентите
+        // private string name;
+        // после пишем this.name = name;
+        // и после отдолу е public string Name { get => name; }
+
+        private string name;
+        private int age;
+        // ще капсулирам AvarageGrades
+        private readonly Dictionary<string, double> avarageGrades;
+        public Student(string name, int age)
+        {
+            Name = name;
+            Age = age;
+            this.avarageGrades = new Dictionary<string, double>();
+        }
+        // ще трябва да валидираме приети стойности (имена или години) 
+        // не може да приемем отрицателни години например
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            private set
+            {
+                if (string.IsNullOrEmpty(value) || value.Length < 2) // така не мога да го създам с null или празно
+                    // 2рия въпрос е за под 2 букви да не е валидна стойност
+                {
+                    throw new ArgumentException("Name is invalid");
+                }
+                name = value;
+            }
+        }
+        // protected int Age може да бъде достъпен от Junior но не и от Program
+        protected int Age
+        {
+            get
+            {
+                return age;
+            }
+            set
+            {
+                if (value < 18)
+                {
+                    throw new ArgumentException("Age is invalid");
+                }
+                age = value;
+            }
+        }
+        // така написахме че да влизаме в Dictinary с оценките но да не използваме 
+        /*
+        public Dictionary<string, double> AvarageGrades 
+        { 
+            get => avarageGrades;
+            set => avarageGrades = value;
+        }
+        */
+        public void AddAvarageGrade(string subject, double avarageGrade)
+        {
+            if (string.IsNullOrWhiteSpace(subject))
+            {
+                throw new ArgumentException("Invalid Subject");
+            }
+            if (avarageGrade < 2 || avarageGrade > 6)
+            {
+                throw new ArgumentException("Invalid AvarageGrade");
+            }
+            avarageGrades.Add(subject, avarageGrade);
+        }
+        public void PrintAvarageGrade()
+        {
+            foreach (var item in avarageGrades)
+            {
+                Console.WriteLine(item.Key + ": " + item.Value);
+            }
+        }
+        public void DoClear()
+        {
+            // ако нямаме горе private readonly Dictionary<string, double> avarageGrades;
+            // то ние ще може да презапишем нова бибютека която е празна
+            // avarageGrades = new Dictionary<string, double>();
+
+            // но по този начин ще може да влезнем и изтрием
+            avarageGrades.Clear();
+        }
+        public Student GetCurrent()
+        {
+            return this;
+        }
+    }
+}
+public class Junior : Student
+{
+    public Junior(string name, int age) : base(name, age)
+    {
+    }
+    public void DoJunior()
+    {
+        Age = 20;
+    }
+}
