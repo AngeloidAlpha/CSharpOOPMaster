002728d06bfb018a3de547cc144aa9a9bbb1d5c3
AngeloidAlpha <angeloid_alpha@abv.bg>
2024-07-28 15:57:31 +0300

    28.July 1st

---
+++ 02.Exercise/02.Encapsulation/Box/Box.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Box
+{
+    public class Box
+    {
+        // Option #1 - get-only properties
+        /*
+        public Box(double length, double width, double height)
+        {
+            // условията за нулеви и негативни стойности
+            if (length <= 0) throw new ArgumentException($"{nameof(Length)} cannot be zero or negative.");
+            if (width <= 0) throw new ArgumentException($"{nameof(Width)} cannot be zero or negative.");
+            if (height <= 0) throw new ArgumentException($"{nameof(Height)} cannot be zero or negative.");
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+        // създаваме параметрите ни
+        public double Length { get; }
+        public double Width { get; }
+        public double Height { get; }
+        */
+
+        // Option #2 - standart properties, but private set
+        // това е опция само в случаите които трябва да имам set-ер + валидация на параметрите към тоя set-er
+        // които да се изпълняват във всяка една промяна на параметъра
+        // тука си пишем фиилдовете на един ред те може да се манипулират при не-перфектен input за да заложим
+        // перфектен input за програмата ни в property-тата ни Length (с главна буква)
+        private double _length, _width, _height;
+        public Box(double length, double width, double height)
+        {
+            Length = length;
+            Width = width;
+            Height = height;
+        }
+        public double Length 
+        {
+            get => this._length;
+            private set
+            {
+                if (_length <= 0) throw new ArgumentException($"{nameof(Length)} cannot be zero or negative.");
+                this._length = value;
+            }
+        }
+        public double Width
+        {
+            get => this._width;
+            private set
+            {
+                if (_width <= 0) throw new ArgumentException($"{nameof(Width)} cannot be zero or negative.");
+                this._width = value;
+            }
+        }
+        public double Height
+        {
+            get => this._height;
+            private set
+            {
+                if (_height <= 0) throw new ArgumentException($"{nameof(Height)} cannot be zero or negative.");
+                this._height = value;
+            }
+        }
+        public double SurfaceArea() => this.LateralSurfaceArea() + 2 * this.Length * this.Width;
+
+        // пресмятане на лицето, всяка страна дължина по височина (и те са 3 такива еднакви страни които се дублират горе/долу)
+        public double LateralSurfaceArea() => 2 * (this.Length * this.Height + this.Width * this.Height);
+        public double Volume() => this.Length * this.Width * this.Height;
+    }
+}
---
+++ 02.Exercise/02.Encapsulation/Box/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Box;
+public static class Program
+{
+    public static void Main(string[] args)
+    {
+        // искаме да видим за подадените ни данни
+        double length = double.Parse(Console.ReadLine());
+        double width = double.Parse(Console.ReadLine());
+        double height = double.Parse(Console.ReadLine());
+
+        try
+        {
+            // създаваме кутия с тези параметри
+            // ако създам кутията преди да съм погледнал параметрите си "try" в Box класа то ще ми ги валидира с негативни
+            // и покаже грешка на ред който е с проверката
+            Box box = new Box(length, width, height);
+            // изписваме информацията за тази кутия
+            Console.WriteLine($"Surface Area - {box.SurfaceArea():f2}");
+            Console.WriteLine($"Lateral Surface Area - {box.LateralSurfaceArea():f2}");
+            Console.WriteLine($"Volume - {box.Volume():f2}");
+        }
+        // трябва да изпишем въпрос за негативни подадени данни (ще изпише грешка ако не го направим)
+        // питаме с try ако всичко е валидно изпиши горните
+        // в противен случай catch (да вземе дадения throw в параметър) и да достъпим неговото съобщение с e.Message
+        catch (ArgumentException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}
